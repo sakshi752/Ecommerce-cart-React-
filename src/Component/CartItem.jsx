33a47a90ca11d217ab8f61baacf3f07cdf86ac99
Sakshi <saks0705@gmail.com>
2026-06-08 15:52:21 +0530

    cart item design and remove from cart

--- src/Component/CartItem.jsx
+++ src/Component/CartItem.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
+import { MdCancel } from "react-icons/md";
+import { RiSubtractFill} from "react-icons/ri";
+import { IoAdd } from "react-icons/io5";
 
-const CartItem = () => {
+const CartItem = ({ id, title, quantity, price, image, category,handleRemove }) => {
     return (
         <div className='bg-blue-950  flex rounded-md text-white shadow-lg'>
-            <div className='w-[30%]'><img src="/images/img1.jpg" alt="" className='rounded-l-md object-cover' /></div>
-            <div className='flex flex-col w-[70%] py-4 px-3'>
+            <div className='w-[30%] h-32'>
+                <img src={image} alt="" className="w-full h-full object-cover rounded-l-md" />
+
+            </div>
+            <div className='flex flex-col w-[70%] pt-4 px-3'>
+
                 <div className='flex justify-between text-xl font-bold '>
-                    <span className=''>Title</span>
-                    <span>$100</span>
+                    <span className=''>{title}</span>
+                    <MdCancel size={30} className='mb-1 cursor-pointer text-pink-300' onClick={handleRemove} />
                 </div>
                 <div className='flex justify-between items-center'>
-                    <span>category</span>
-                    <div className='flex items-center justify-center'>
-                        <span>-</span>
-                        <span>1</span>
-                        <span>+</span>
-                    </div>
 
+                    <div className="flex items-center gap-2">
+                        <button
+                            className="w-7 h-7 flex items-center justify-center bg-white text-black rounded cursor-pointer text-xl font-bold"
+                        >
+                            <RiSubtractFill/>
+                        </button>
+
+                        <span className="min-w-6 text-center font-semibold">
+                            {quantity}
+                        </span>
+
+                        <button
+                            className="w-7 h-7 flex items-center justify-center bg-white text-black rounded cursor-pointer"
+                        >
+                            <IoAdd/>
+                        </button>
+                    </div>
+                    <span>${price}</span>
                 </div>
             </div>
 
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,17 +1,41 @@
 import React, { useEffect, useState } from 'react'
 import CartItem from '../Component/CartItem';
+import { toast } from 'react-toastify';
 
 const Cart = () => {
-  const [cartItems,setCartItems] = useState(()=>{
+  const [cartItems, setCartItems] = useState(() => {
     return JSON.parse(localStorage.getItem("cart")) || [];
   })
-  
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const handleRemove = (productId, title) => {
+    setCartItems(prev => {
+      return prev.filter(item => item.id != productId)
+    })
+    toast.success(`${title} is removed from cart 🛒`);
+
+  }
+
   return (
-    <div>
-      <div className='w-[30%]'>
-        
-      </div>
-    </div>
+    <>
+      {cartItems.length === 0 ? <div className='flex items-center justify-center'>
+        <p className='text-2xl font-bold'>Your cart is empty!</p>
+      </div> : <div className='w-[30%] flex flex-col gap-7'>
+        <div><p className='text-2xl font-bold text-white'>Your Cart</p></div>
+        <div className='flex flex-col gap-10'>
+          {cartItems.map(item => {
+            return (
+              <CartItem key={item.id} id={item.id} title={item.title} quantity={item.quantity} price={item.price} image={item.image} category={item.category} handleRemove={() => handleRemove(item.id, item.title)} />
+            )
+          })}
+        </div>
+      </div>}
+    </>
+
+
   )
 }
 
